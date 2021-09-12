c36c9a9704a37fb4a5d831de6f01a473e69a9cf0
mayone-du <cocomayo1201@icloud.com>
2021-09-12 09:20:59 +0900

    Add get href attributes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
+use select::document::Document;
+use select::predicate::Name;
+
 fn main() -> eyre::Result<()> {
     let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
 
-    println!("body = {:?}", body);
+    let doc = Document::from(body.as_str());
+    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+        println!("{:?}", href);
+    }
 
     Ok(())
 }
