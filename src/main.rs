7701af96c5d2110e2e3877b27c001e0e7f2abe29
mayone-du <cocomayo1201@icloud.com>
2021-11-13 09:35:33 +0900

    Update

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,20 @@
 use select::document::Document;
 use select::predicate::Name;
+use std::env;
 
 fn main() -> eyre::Result<()> {
-    let body = reqwest::blocking::get("https://www.rust-lang.org")?.text()?;
+    let args: Vec<String> = env::args().collect();
+
+    let url = if args.len() > 1 {
+        &args[1]
+    } else {
+        "https://www.rust-lang.org/en-US/downloads.html"
+    };
+
+    let body = reqwest::blocking::get(url)?.text()?;
 
     let doc = Document::from(body.as_str());
-    for href in doc.find(Name("a")).filter_map(|a| a.attr("href")) {
+    for href in doc.find(Name("meta")).filter_map(|a| a.attr("content")) {
         println!("{:?}", href);
     }
 
